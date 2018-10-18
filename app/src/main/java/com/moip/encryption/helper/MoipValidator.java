61cd198dc84dc01639db71909c0998f908678253
JeanJNAP <jeanjnap@gmail.com>
2018-10-18 10:45:13 -0300

    Moip Implementation

---
+++ app/src/main/java/com/moip/encryption/helper/MoipValidator.java
@@ -0,0 +1,134 @@
+package com.android.moip.encryption.helper;
+
+import br.com.moip.creditcard.Brands;
+import com.android.moip.encryption.entities.types.CreditCardBrand;
+import br.com.moip.validators.CreditCard;
+
+public class MoipValidator {
+
+    public static Brands brand(final String ccNumber) {
+        return new CreditCard(ccNumber).getBrand();
+    }
+
+    public static boolean isValid(final String ccNumber) {
+        return new CreditCard(ccNumber).isValid();
+    }
+
+    public static boolean isValidCVC(final String cvc) {
+        return (isNumber(cvc) && (cvc.length() == 3 || cvc.length() == 4));
+    }
+
+    public static boolean isValidMonth(final String expirationMonth) {
+        if (expirationMonth.length() != 1 && expirationMonth.length() != 2)
+            return false;
+
+        if (!isNumber(expirationMonth))
+            return false;
+
+        int month = Integer.parseInt(expirationMonth);
+
+        return (month > 0 && month <= 12);
+    }
+
+    public static boolean isValidYear(final String expirationYear) {
+        if (expirationYear.length() != 2 && expirationYear.length() != 4)
+            return false;
+
+        if (!isNumber(expirationYear))
+            return false;
+
+        int year = Integer.parseInt(expirationYear);
+
+        if (expirationYear.length() == 4) {
+            if (year < 1000 || year >= 3000)
+                return false;
+        } else {
+            if (year < 0 || year > 99)
+                return false;
+        }
+        return true;
+    }
+
+    private static boolean isNumber(final String text) {
+        return (text.matches("[0-9]+"));
+    }
+
+    @Deprecated
+    public static boolean isValidCreditCard(final String ccNumber) {
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = ccNumber.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(ccNumber.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n = (n % 10) + 1;
+                }
+            }
+            sum += n;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
+    }
+
+    @Deprecated
+    public static CreditCardBrand verifyBrand(String ccNumber) {
+        if (ccNumber.matches("^([6362]{4})([0-9]{12})$"))
+            return CreditCardBrand.ELO;
+        else if (ccNumber.matches("^4[0-9]{12}(?:[0-9]{3})?$"))
+            return CreditCardBrand.VISA;
+        else if (ccNumber.matches("^5[1-5][0-9]{14}$"))
+            return CreditCardBrand.MASTERCARD;
+        else if (ccNumber.matches("^3[47][0-9]{13}$"))
+            return CreditCardBrand.AMEX;
+        else if (ccNumber.matches("^3(?:0[0-5]|[68][0-9])[0-9]{11}$"))
+            return CreditCardBrand.DINERS;
+        else
+            return CreditCardBrand.UNKNOWN;
+    }
+
+    @Deprecated
+    public static CreditCardBrand quicklyBrand(final String ccNumber) {
+        if (ccNumber.length() < 2) {
+            return CreditCardBrand.UNKNOWN;
+        }
+
+        Integer range = Integer.valueOf(ccNumber.substring(0, 2));
+
+        if (range >= 40 && range <= 49) {
+            return CreditCardBrand.VISA;
+        } else if (range >= 50 && range <= 59) {
+            return CreditCardBrand.MASTERCARD;
+        } else if (range == 34 || range == 37) {
+            return CreditCardBrand.AMEX;
+        } else if (range == 60 || range == 62 || range == 64 || range == 65) {
+            return CreditCardBrand.DISCOVER;
+        } else if (range == 36 || range == 38) {
+            return CreditCardBrand.DINERS;
+        } else {
+            if (ccNumber.length() < 3)
+                return CreditCardBrand.UNKNOWN;
+
+            range = Integer.valueOf(ccNumber.substring(0, 3));
+            if (range == 301 || range == 305)
+                return CreditCardBrand.DINERS;
+
+            if (ccNumber.length() < 4)
+                return CreditCardBrand.UNKNOWN;
+
+            range = Integer.valueOf(ccNumber.substring(0, 4));
+            if (range == 5067 || range == 4576 || range == 4011)
+                return CreditCardBrand.ELO;
+
+            if (ccNumber.length() < 6)
+                return CreditCardBrand.UNKNOWN;
+
+            range = Integer.valueOf(ccNumber.substring(0, 6));
+            if (range == 636368 || range == 636369 || range == 438935 || range == 504175
+                    || range == 451416 || range == 636297 || range == 506699)
+                return CreditCardBrand.ELO;
+
+            return CreditCardBrand.UNKNOWN;
+        }
+    }
+}
--- app/src/main/java/com/moip/encryption/jeanjnap/moipencryptionteste/MainActivity.java
+++ app/src/main/java/com/moip/encryption/jeanjnap/moipencryptionteste/MainActivity.java
@@ -1,13 +1,27 @@
 package com.moip.encryption.jeanjnap.moipencryptionteste;
 
+import android.content.DialogInterface;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAopPr9r5KIWy0kw8jDnJN\n" +
+            "uhY4mRRCEiplG+oMjTIdsML2k1etApYccuwK/qbgToxPMuAXwX3i3EkATFkWCwCG\n" +
+            "QxQvfwBIzaWCDA6qE0dMBHOu4jDmuaQWa5MnnQhFoPkjGC+axmNFzMxzIm6xWI1c\n" +
+            "mtsmt9dO7VOZbkcwa9s2K9d7fjQg351zcU51KiW5RHKl1jmjkjvOWmu+UmTMRthB\n" +
+            "MdJv5n+1O4JxO5L2S0IrhKKV2H0uPyFRkpMrCV5gfFWhNV17FKPM3ZdH+I1ZIEvj\n" +
+            "IugRtZYuaqDvulQHFxwMqsAC9fHrwq8AgRgHgmHly9QGXPOyzuvJJGbjGMoI0H2Z\n" +
+            "ZwIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
 
     TextInputEditText number, cvc, expM, expY;
     Button encrypt;
@@ -38,6 +52,37 @@ public class MainActivity extends AppCompatActivity {
 
     private void criptografar() {
         
+        com.android.moip.encryption.entities.CreditCard creditCard = new com.android.moip.encryption.entities.CreditCard();
+        creditCard.setCvc("123");
+        creditCard.setNumber("5555666677778884");
+        creditCard.setExpirationMonth("12");
+        creditCard.setExpirationYear("2018");
+        creditCard.setPublicKey(PUBLIC_KEY);
+
+        String hashTxt = "";
+
+        try{
+            hashTxt = creditCard.encrypt();
+            Log.i("RESULT", "Hash: \n"+ hashTxt);
+            hash.setText(hashTxt);
+        }catch(com.android.moip.encryption.exception.MoipEncryptionException mee){
+            Log.i("RESULT", "Erro ao criptografar cartão:\n" + mee.getMessage());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            //define o titulo
+            builder.setTitle("Erro");
+            //define a mensagem
+            builder.setMessage(mee.getMessage());
+            //define um botão como positivo
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+
+                }
+            });
+
+            builder.show();
+
+        }
     }
 
 

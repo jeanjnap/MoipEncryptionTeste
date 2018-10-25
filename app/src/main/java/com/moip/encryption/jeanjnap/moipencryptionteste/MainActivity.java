37427a1a18340773a6703fed27408f41095165ba
JeanJNAP <jeanjnap@gmail.com>
2018-10-25 14:36:01 -0300

    Add o moip pelo .jar

--- app/src/main/java/com/moip/encryption/jeanjnap/moipencryptionteste/MainActivity.java
+++ app/src/main/java/com/moip/encryption/jeanjnap/moipencryptionteste/MainActivity.java
@@ -1,6 +1,8 @@
 package com.moip.encryption.jeanjnap.moipencryptionteste;
 
+import android.content.DialogInterface;
 import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import br.com.moip.encryption.entities.CreditCard;
+import br.com.moip.encryption.exception.MoipEncryptionException;
+
+
 public class MainActivity extends AppCompatActivity {
 
     TextInputEditText number, cvc, expM, expY;
     Button encrypt;
     TextView hash, brand;
+
+    private static final String PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAopPr9r5KIWy0kw8jDnJN\n" +
+            "uhY4mRRCEiplG+oMjTIdsML2k1etApYccuwK/qbgToxPMuAXwX3i3EkATFkWCwCG\n" +
+            "QxQvfwBIzaWCDA6qE0dMBHOu4jDmuaQWa5MnnQhFoPkjGC+axmNFzMxzIm6xWI1c\n" +
+            "mtsmt9dO7VOZbkcwa9s2K9d7fjQg351zcU51KiW5RHKl1jmjkjvOWmu+UmTMRthB\n" +
+            "MdJv5n+1O4JxO5L2S0IrhKKV2H0uPyFRkpMrCV5gfFWhNV17FKPM3ZdH+I1ZIEvj\n" +
+            "IugRtZYuaqDvulQHFxwMqsAC9fHrwq8AgRgHgmHly9QGXPOyzuvJJGbjGMoI0H2Z\n" +
+            "ZwIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +61,38 @@ public class MainActivity extends AppCompatActivity {
         numberTxt = String.valueOf(number.getText());
         expMTxt = String.valueOf(expM.getText());
         expYTxt = String.valueOf(expY.getText());
-        
-
-
 
         String hashTxt = "";
 
+        CreditCard creditCard = new CreditCard();
+        creditCard.setCvc(cvcTxt);
+        creditCard.setNumber(numberTxt);
+        creditCard.setExpirationMonth(expMTxt);
+        creditCard.setExpirationYear(expYTxt);
+        creditCard.setPublicKey(PUBLIC_KEY);
+
+        try{
+            hashTxt = creditCard.encrypt();
+            Log.i("RESULT", "Hash: \n"+ hashTxt);
+            hash.setText(hashTxt);
+        }catch(MoipEncryptionException mee){
+            Log.i("RESULT", "Erro ao criptografar cartão:\n" + mee.getMessage());
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            //define o titulo
+            builder.setTitle("Erro");
+            //define a mensagem
+            builder.setMessage(mee.getMessage());
+            //define um botão como positivo
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+
+                }
+            });
+
+            builder.show();
+
+        }
 
     }
 
